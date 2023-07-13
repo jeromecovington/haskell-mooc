607037abe8f6fc9962e1bef0c40bba1d93fa7970
Jerome Covington <jeromecovington@gmail.com>
2023-07-12 23:38:32 -0400

    6/11 passing

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -91,7 +91,10 @@ isPalindrome str = str == reverse str
 --   palindromify "abracacabra" ==> "acaca"
 
 palindromify :: String -> String
-palindromify s = todo
+palindromify s
+  | null s = s
+  | s == reverse s = s
+  | otherwise = palindromify(drop 1 (take (length s - 1) s))
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement safe integer division, that is, a function that
